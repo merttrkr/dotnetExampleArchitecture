6cad612d9c9bcd7df004307fc491f0434d17b2fc
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-09-05 14:21:16 +0300

    Create Product.cs

---
+++ src/dotnetExampleArchitecture/Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product:Entity<Guid>
+    {
+        public string? Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        //categoryId naming is special it is a foreign key naming
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
